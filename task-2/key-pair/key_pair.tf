c40b088f36d1e9e29b5a260a8caef8a570e4bf85
zzainesh <zzainesh@dar.io>
2024-10-14 02:39:57 +0500

    change key_pair

--- task-2/key-pair/key_pair.tf
+++ task-2/key-pair/key_pair.tf
@@ -1,4 +1,10 @@
+resource "tls_private_key" "pk_key" {
+  algorithm = "RSA"
+  rsa_bits  = 2048
+}
+
 resource "aws_key_pair" "key_rsschool" {
   key_name   = "key-rsschool"
-  public_key = file("~/.ssh/id_rsa.pub")
+  public_key = tls_private_key.pk_key.public_key_openssh
 }
+
