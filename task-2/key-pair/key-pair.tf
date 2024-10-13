96e04ad005d46661670bad30b04653558499d591
zzainesh <zzainesh@dar.io>
2024-10-14 03:56:28 +0500

    create file pub

--- task-2/key-pair/key-pair.tf
+++ task-2/key-pair/key-pair.tf
@@ -1,14 +1,4 @@
-resource "tls_private_key" "pk_key" {
-  algorithm = "RSA"
-  rsa_bits  = 2048
-}
-
 resource "aws_key_pair" "key_pair_rsschool" {
   key_name   = "key-pair-rsschool"
-  public_key = tls_private_key.pk_key.public_key_openssh
-}
-
-output "private_key" {
-  value     = tls_private_key.my_key.private_key_pem
-  sensitive = true
+  public_key = file("task-2/key-pair/id_rsa.pub")
 }
