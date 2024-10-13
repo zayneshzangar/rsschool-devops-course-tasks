0ad817100152eb5066b1489c7b1d2968a7e671f5
zzainesh <zzainesh@dar.io>
2024-10-14 03:37:51 +0500

    change name key

--- task-2/key-pair/key-pair.tf
+++ task-2/key-pair/key-pair.tf
@@ -3,7 +3,7 @@ resource "tls_private_key" "pk_key" {
   rsa_bits  = 2048
 }
 
-resource "aws_key_pair" "key_rsschool" {
-  key_name   = "key-rsschool"
+resource "aws_key_pair" "key_pair_rsschool" {
+  key_name   = "key-pair-rsschool"
   public_key = tls_private_key.pk_key.public_key_openssh
 }
