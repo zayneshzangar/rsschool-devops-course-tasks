7340fb0620c4982d8dc8d3bb46df5541c05b8b63
zzainesh <zzainesh@dar.io>
2024-10-14 03:47:27 +0500

    output key

--- task-2/key-pair/key-pair.tf
+++ task-2/key-pair/key-pair.tf
@@ -7,3 +7,8 @@ resource "aws_key_pair" "key_pair_rsschool" {
   key_name   = "key-pair-rsschool"
   public_key = tls_private_key.pk_key.public_key_openssh
 }
+
+output "private_key" {
+  value     = tls_private_key.my_key.private_key_pem
+  sensitive = true
+}
